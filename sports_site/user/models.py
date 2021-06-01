3b8f3b10cb8690ae7691c7e3e55a0ade5f9bf562
Cole <cebCole@gmail.com>
2021-06-01 13:51:05 +0000

    UserProfile onetooone to tie a league field to each user

--- sports_site/user/models.py
+++ sports_site/user/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import User
 from django.db import models
+from league.models import League
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    league = models.ForeignKey(League, on_delete=models.CASCADE, null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
