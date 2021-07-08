c62be62a8385e29faef0c10684a7dfbc02120037
Cole <cebCole@gmail.com>
2021-07-08 11:48:08 +0000

    fixed IP validator bug, fixed validator text type and help text

--- sports_site/stats/validators.py
+++ sports_site/stats/validators.py
@@ -2,9 +2,9 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 def validate_innings_pitched(value):
-    decimal = round(value % 1)
-    if decimal not in [0.1, 0.2]:
+    decimal = round(value % 1, 1)
+    if decimal not in [0, 0.1, 0.2]:
         raise ValidationError(
-            _('%(decimal)s is not properly fomratted. Innings Pitched value should .1 or .2'),
+            _('%(decimal)s is not properly formatted. Innings Pitched decimal value should .1 or .2'),
             params={'decimal': decimal},
             )
