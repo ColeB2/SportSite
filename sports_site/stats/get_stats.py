38242fd73138cb09768015a65bee256f0c0b257f
Cole <cebCole@gmail.com>
2021-07-22 13:55:13 +0000

    links to player page from league leaders widget

--- sports_site/stats/get_stats.py
+++ sports_site/stats/get_stats.py
@@ -8,6 +8,7 @@ def get_league_leaders(league, featured_stage):
     """Returns league leaders in Avg, HomeRuns, RBI, SB and Runs"""
     hitting_stats = PlayerHittingGameStats.objects.all().filter(player__player__league=league, season=featured_stage)
     hitting_stats1 = hitting_stats.values("player").annotate(
+        player_id = F("player__player__pk"),
         first = F("player__player__first_name"),
         last = F("player__player__last_name"),
         team = F("player__team__team__team__name"),
