ad8cc069e16a2876c2b674eab9aefcc863ae53f8
Cole <cebCole@gmail.com>
2021-07-22 12:33:33 +0000

    code refactoring

---
+++ sports_site/stats/get_stats.py
@@ -0,0 +1,22 @@
+from django.db.models import  F, FloatField, Sum
+from django.db.models.functions import Cast
+from .models import PlayerHittingGameStats
+
+
+
+def get_league_leaders(league, featured_stage):
+    """Returns league leaders in Avg, HomeRuns, RBI, SB and Runs"""
+    hitting_stats = PlayerHittingGameStats.objects.all().filter(player__player__league=league, season=featured_stage)
+    hitting_stats1 = hitting_stats.values("player").annotate(
+        first = F("player__player__first_name"),
+        last = F("player__player__last_name"),
+        team = F("player__team__team__team__name"),
+        at_bats = Sum('at_bats'),
+        runs = Sum('runs'),
+        hits = Sum('hits'),
+        homeruns = Sum('homeruns'),
+        runs_batted_in = Sum('runs_batted_in'),
+        stolen_bases = Sum('stolen_bases'),
+        average = Cast(F('hits'),FloatField())/ Cast(F('at_bats'), FloatField())
+        )
+    return hitting_stats1
