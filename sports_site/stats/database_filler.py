741a59f9c6e5c6c002026738c42c2edc2f077098
Cole <cebCole@gmail.com>
2021-08-02 12:06:43 +0000

    get_league and get_featuerd_season_stage func used to fill database w data

--- sports_site/stats/database_filler.py
+++ sports_site/stats/database_filler.py
@@ -1,10 +1,21 @@
-from league.models import Game, TeamSeason, SeasonStage
+from league.models import League, Game, TeamSeason, SeasonStage
+from stats.models import TeamGameLineScore, TeamGameStats
 from random import randint
 '''
 dates = [datetime.date(2021,5,14), datetime.date(2021,5,16),datetime.date(2021,5,21),datetime.date(2021,5,23),datetime.date(2021,5,28),datetime.date(2021,5,30),datetime.date(2021,6,4),datetime.date(2021,6,6),datetime.date(2021,6,11),datetime.date(2021,6,13),datetime.date(2021,6,18),datetime.date(2021,6,20),datetime.date(2021,6,25),datetime.date(2021,6,27),datetime.date(2021,7,2),datetime.date(2021,7,4)]
 g = [[41,52,36], [21,43,65],[13,62,54],[16,35,24],[15,64,32],[41,25,63],[12,34,56],[31,26,45],[61,53,42],[51,46,23],[14,52,36],[21,43,65],[13,62,54],[16,35,24],[15,64,32],[41,25,63]]
 td = {'2':teams[0], '1':teams[1], '3':teams[2], '4':teams[3], '5':teams[4], '6':teams[5]}
 '''
+
+def get_league(url="SBBL"):
+    league = League.objects.get(url=url)
+    return league
+
+def get_featured_season_stage(league=None):
+    season_stage = SeasonStage.objects.get(featured=True, season__league=league)
+    return season_stage
+
+
 def create_schedule(dates, games, team_dict):
     """Creates a schedule given list of dates, a same len list of games for said
     date, and a team_dict to map numbers from games to teamseason objects.
