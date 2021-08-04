64a9082f22891a159d8a51323a5e54eb3ff7203d
Cole <cebCole@gmail.com>
2021-08-04 12:38:51 +0000

    basic team info page

--- sports_site/league/urls.py
+++ sports_site/league/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('player/<int:player_pk>', views.player_page_view, name='player-page'),
-    path('schedule/', views.schedule_view, name="schedule-page"),
+    path('team/<int:team_pk>', views.team_page_view, name='team-page'),
+    path('schedule/', views.schedule_page_view, name="schedule-page"),
     ]
--- sports_site/league/views.py
+++ sports_site/league/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from .models import Game, League, Player, PlayerSeason, SeasonStage
+from .models import (Game, League, Player, PlayerSeason, SeasonStage, Team,
+    TeamSeason)
 
-# Create your views here.
+
 def player_page_view(request, player_pk):
     player = Player.objects.get(pk=player_pk)
     league = player.league
     player_seasons = PlayerSeason.objects.all().filter(player=player)
+
     context = {
         "league": league,
         "player": player,
@@ -14,7 +16,7 @@ def player_page_view(request, player_pk):
     return render(request, "league/player_page.html", context)
 
 
-def schedule_view(request):
+def schedule_page_view(request):
     league_slug = request.GET.get('league', None)
     league = League.objects.get(url=league_slug)
     featured_stage = SeasonStage.objects.get(season__league=league, featured=True)
@@ -26,3 +28,19 @@ def schedule_view(request):
         "featured_stage": featured_stage,
     }
     return render(request, "league/schedule_page.html", context)
+
+
+def team_page_view(request, team_pk):
+    team = Team.objects.get(pk=team_pk)
+    league = team.league
+    featured_stage = SeasonStage.objects.get(season__league=league, featured=True)
+
+    context = {
+        "featured_stage": featured_stage,
+        "league": league,
+        "team": team,
+
+        }
+    return render(request, "league/team_page.html", context)
+
+
