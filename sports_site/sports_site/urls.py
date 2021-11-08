2664cefd9b6049dd5335531f0729293de3876029
Cole <cebCole@gmail.com>
2021-11-08 13:25:16 +0000

    url for password reset confirm/ complete and html for confirm

--- sports_site/sports_site/urls.py
+++ sports_site/sports_site/urls.py
@@ -24,6 +24,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name='user/password_reset.html'), name='password-reset'),
+    path('password-reset/confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='user/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='user/password_reset_complete.html'), name='password_reset_complete'),
     path('league/', include("news.urls")),
     path('league/roster/', include("user.urls")),
     path('league/admin/', include("league_admin.urls")),
