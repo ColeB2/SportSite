689e231e59b909e4c4ebb625609559dd3124caed
Cole <cebCole@gmail.com>
2021-11-09 13:52:51 +0000

    commit

--- sports_site/user_auth/views.py
+++ sports_site/user_auth/views.py
@@ -5,9 +5,9 @@ from django.contrib.auth.views import PasswordResetView
 from django.contrib.messages.views import SuccessMessageMixin
 
 class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
-    template_name = 'user/password_reset.html'
-    email_template_name = 'user/password_reset_email.html'
-    # subject_template_name = 'user/password_reset_subject'
+    template_name = 'user_auth/password_reset.html'
+    email_template_name = 'user_auth/password_reset_email.html'
+    subject_template_name = 'user_auth/password_reset_subject.txt'
     success_message = "We've emailed you instructions for setting your password, " \
                       "if an account exists with the email you entered. You should receive them shortly." \
                       " If you don't receive an email, " \
@@ -16,6 +16,6 @@ class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
 
 
 
-class RegisterView(CreateView):
-    pass
+# class RegisterView(CreateView):
+#     pass
 
