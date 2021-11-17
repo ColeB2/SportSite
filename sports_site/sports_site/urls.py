ab1bc8c47fd129ab20b481141e691f41b1bdda2e
Cole <cebCole@gmail.com>
2021-11-17 12:28:07 +0000

    removed commented out url

--- sports_site/sports_site/urls.py
+++ sports_site/sports_site/urls.py
@@ -26,7 +26,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(template_name='user_auth/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user_auth/logout.html'), name='logout'),
-    # path('password-reset/', auth_views.PasswordResetView.as_view(template_name='user_auth/password_reset.html'), name='password-reset'),
     path('password-reset/', ResetPasswordView.as_view(), name='password-reset'),
     path('password-reset/confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='user_auth/password_reset_confirm.html'), name='password_reset_confirm'),
     path('password-reset/complete/', auth_views.PasswordResetCompleteView.as_view(template_name='user_auth/password_reset_complete.html'), name='password_reset_complete'),
