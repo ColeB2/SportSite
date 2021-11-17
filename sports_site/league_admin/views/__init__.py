2f6e640ec43570d3c47419edbacf6521841e7ebb
Cole <cebCole@gmail.com>
2021-11-17 12:55:13 +0000

    moved seasonstage, teamseason views to seasonstage views file and corresponding changes

--- sports_site/league_admin/views/__init__.py
+++ sports_site/league_admin/views/__init__.py
@@ -1,6 +1,7 @@
 from .games_views import *
 from .player_views import *
 from .season_views import *
+from .season_stage_views import *
 from .team_views import *
 from .user_views import *
 from .views import *
