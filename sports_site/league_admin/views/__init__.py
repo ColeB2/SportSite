e15a7b2bcd58fdddfc2366571effd3cef0246501
Cole <cebCole@gmail.com>
2021-11-17 12:57:58 +0000

    updated league admin views init

--- sports_site/league_admin/views/__init__.py
+++ sports_site/league_admin/views/__init__.py
@@ -3,5 +3,6 @@ from .player_views import *
 from .season_views import *
 from .season_stage_views import *
 from .team_views import *
+from .team_season_views import *
 from .user_views import *
 from .views import *
