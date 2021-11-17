4d1121f9df2790022ca11012b3718433f80598d8
Cole <cebCole@gmail.com>
2021-11-17 12:31:09 +0000

    league_admin/views/user_views.py minor refactors/spacing changes

--- sports_site/league_admin/views/user_views.py
+++ sports_site/league_admin/views/user_views.py
@@ -31,21 +31,26 @@ def league_admin_user_info_view(request, user_name, user_pk):
         }
     return render(request, "league_admin/user_info.html", context)
 
+
 @permission_required('league.league_admin')
 def league_admin_user_edit_perms_view(request, user_name, user_pk):
     user_viewed = User.objects.get(pk=user_pk)
     team = Team.objects.get(owner=user_viewed, league__admin=request.user)
 
-    roster_permissions = Permission.objects.filter(content_type__app_label=Roster._meta.app_label,
-        content_type__model=Roster._meta.model_name, codename__contains='user')
+    roster_permissions = Permission.objects.filter(
+        content_type__app_label=Roster._meta.app_label,
+        content_type__model=Roster._meta.model_name,
+        codename__contains='user')
     user_permissions = Permission.objects.filter(user__pk=user_pk)
 
     if request.method =="POST":
-        form = EditUserRosterPermissionsForm(data=request.POST, selected_user=user_viewed)
+        form = EditUserRosterPermissionsForm(data=request.POST,
+                                             selected_user=user_viewed)
         if form.is_valid():
             if form.has_changed():
                 form.process()
-                messages.success(request, f"{user_viewed}'s permissions updated.")
+                messages.success(request,
+                                 f"{user_viewed}'s permissions updated.")
         return redirect('league-admin-user-info', user_name, user_pk)
     else:
         form = EditUserRosterPermissionsForm(selected_user=user_viewed)
