528db795f7d4d05b02317e54e8ed475b7ce5cef7
Cole <cebCole@gmail.com>
2021-11-22 12:28:31 +0000

    fixed too long lines in player_views

--- sports_site/league_admin/views/player_views.py
+++ sports_site/league_admin/views/player_views.py
@@ -30,7 +30,8 @@ def league_admin_player_create_view(request):
         "form": form,
         "league": league,
     }
-    return render(request, "league_admin/player_templates/player_create.html", context)
+    return render(request, "league_admin/player_templates/player_create.html",
+                  context)
 
 
 @permission_required('league.league_admin')
@@ -57,7 +58,8 @@ def league_admin_player_select_view(request):
         "league": league,
         }
 
-    return render(request, "league_admin/player_templates/player_select.html", context)
+    return render(request, "league_admin/player_templates/player_select.html",
+                  context)
 
 
 @permission_required('league.league_admin')
@@ -79,7 +81,8 @@ def league_admin_player_edit_view(request, player_pk):
         "form":form,
         "player_instance":player_instance,
         }
-    return render(request, "league_admin/player_templates/player_edit.html", context)
+    return render(request, "league_admin/player_templates/player_edit.html",
+                  context)
 
 
 @permission_required('league.league_admin')
@@ -101,4 +104,5 @@ def league_admin_player_delete_info_view(request, player_pk):
         'player':player,
         'nested_object':nested_object,
     }
-    return render(request, "league_admin/player_templates/player_delete.html", context)
+    return render(request, "league_admin/player_templates/player_delete.html",
+                  context)
