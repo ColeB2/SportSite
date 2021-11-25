9a6b8a05c675dceae9b21960a9737b0f01ff789e
Cole <cebCole@gmail.com>
2021-11-25 12:35:46 +0000

    core/urls.py removed unused url pattern

--- sports_site/core/urls.py
+++ sports_site/core/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path('', views.roster_select, name='')
     path('', views.landing, name='landing'),
     path('<league_url>/', views.find_league, name='league-redirect'),
     path('login/redirect/', views.login_redirect, name='login-redirect'),
