ff9eeac8265f4aa2243cb7d58d721839bd0bd5dc
Cole <cebCole@gmail.com>
2022-01-13 11:16:36 +0000

    LeagueOption admin

--- sports_site/league_admin/admin.py
+++ sports_site/league_admin/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import LeagueOptions
 
 # Register your models here.
+@admin.register(LeagueOptions)
+class LeagueOptionsAdmin(admin.ModelAdmin):
+    list_display = ('stat_options',)
