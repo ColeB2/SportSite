57624361268c9a89d6c2fced60a13eaf1873a00b
Cole <cebCole@gmail.com>
2022-01-17 11:04:32 +0000

    added missing return in OptionsView get_context_data

--- sports_site/league_admin/views/views.py
+++ sports_site/league_admin/views/views.py
@@ -92,6 +92,7 @@ class OptionsView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['options'] = LeagueOptions.objects.get(league=self.league)
+        return context
 
 
 class OptionsFormView(FormView):
