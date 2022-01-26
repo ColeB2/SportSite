1db395ded768767013186a28fb79d58c8a063245
Cole <cebCole@gmail.com>
2022-01-26 12:35:01 +0000

    remove unused code

--- sports_site/league/views.py
+++ sports_site/league/views.py
@@ -29,7 +29,6 @@ def player_page_view(request, player_pk):
     all_stats = get_all_player_season_hitting_stats(player=player, league=league, stage_type=SeasonStage.REGULAR)
     career_stats = get_player_career_hitting_stats(player=player, league=league, stage_type=SeasonStage.REGULAR)
     player_splits = get_stats(qs[:num_games], "last_x_hitting_date")
-    # player_splits = get_player_last_x_hitting_stats(player=player, league=league, num_games=5)
     last_x = [3,5,7]
     last_x_splits = []
     for val in last_x:
