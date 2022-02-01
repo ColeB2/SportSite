7fe38fe6af9ba37caf194199a576640677ae97c4
Cole <cebCole@gmail.com>
2022-02-01 12:38:15 +0000

    starting basic tests for stats

--- sports_site/stats/tests.py
+++ sports_site/stats/tests.py
@@ -1,10 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from league.models import Player, PlayerSeason, Season, SeasonStage, League
 from league.models import PlayerSeason
 
 # Create your tests here.
-class LeagueTestCase(TestCase):
-
+class StatsTestCase(TestCase):
     @classmethod
     def setUpTestData(cls):
-        cls.league = League.objects.create(name="Test League", admin="Test User")
+        cls.user = User.objects.create(username="Test", email="test@email.com", password="test")
+        cls.league = League.objects.create(name="Test League", admin=cls.user, url="TL")
+
+    def test_herehere(self):
+        print(self.league)
+        self.assertEqual(1,1)
+
