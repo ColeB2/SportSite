0daf58c62f7d4d30f3c0bd01e0a1970674d115c1
Cole <cebCole@gmail.com>
2022-02-01 12:11:06 +0000

    basic league.model test cases

--- sports_site/stats/tests.py
+++ sports_site/stats/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from league.models import Player, PlayerSeason, Season, SeasonStage, League
+from league.models import PlayerSeason
 
 # Create your tests here.
+class LeagueTestCase(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.league = League.objects.create(name="Test League", admin="Test User")
