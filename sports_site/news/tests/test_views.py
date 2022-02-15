7550755805744fe40e72573cb432cb7e8df74965
Cole <cebCole@gmail.com>
2022-02-15 12:35:37 +0000

    basic ArticleCreateView Tests

--- sports_site/news/tests/test_views.py
+++ sports_site/news/tests/test_views.py
@@ -43,5 +43,52 @@ class HomeViewTest(TestCase):
         #TodoStats:
         print(response.context["stats"])
 
+
 class NewsDetailTest(TestCase):
-    pass
+    """
+    Tests news_detail from news/views.py
+    """
+    @classmethod
+    def setUpTestData(cls):
+        cls.league = League.objects.get(id=1)
+        cls.article = Article.objects.create(league=cls.league, title="Article Title", body="lorem ipsum", author="Me")
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/league/news/article-title?league=TL')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_accessible_by_name(self):
+        response = self.client.get(reverse('news-detail', args="article-title")+"?league=TL")
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('news-detail', args="article-title")+"?league=TL")
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'news_detail/home.html')
+
+    def test_context(self):
+        response = self.client.get(reverse('news-detail', args="article-title")+"?league=TL")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["league"], self.league)
+        self.assertEqual(response.context["article"], self.article)
+
+
+class ArticleCreateViewTest(TestCase):
+    """
+    Tests ArticleCreateView from news/views.py
+    """
+
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/league/news/create/article')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_accessible_by_name(self):
+        response = self.client.get(reverse('news-create'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        response = self.client.get(reverse('news-create'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'news_detail/home.html')
+
