8701a199631f2a0499d624a0e6a75366b588ccc9
Cole <cebCole@gmail.com>
2022-02-24 13:07:13 +0000

    css changes

--- sports_site/news/tests/test_views.py
+++ sports_site/news/tests/test_views.py
@@ -175,6 +175,9 @@ class ArticleEditViewTest(TestCase):
         response2 = self.client.get(reverse('news-detail', kwargs={"slug":"article-title"})+"?league=TL")
         self.assertEqual(response2.status_code, 200)
 
+        edited_article = Article.objects.All()[0]
+        self.assertEqual(edited_article.title = "TitleOne")
+
         print(self.article.title)
         #self.assertRedirects(response, "/league/?league=TL")
 
