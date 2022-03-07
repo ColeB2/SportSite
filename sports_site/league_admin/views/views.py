7475be4d0a29b0626b263c7d939b63e1ebbdcae6
Cole <cebCole@gmail.com>
2022-03-07 11:15:13 +0000

    options views moved to own view file

--- sports_site/league_admin/views/views.py
+++ sports_site/league_admin/views/views.py
@@ -71,60 +71,7 @@ def league_admin_news_select(request):
         }
     return render(request, "league_admin/article_select.html", context)
 
-def league_admin_options_view(request):
-    league = League.objects.get(admin=request.User)
-    options = LeagueHittingOptions.objects.get(league = league)
 
-    context = {
-        "league": league,
-        "options": options,
-        }
-    return render(request, "league_admin/options.html", context)
-
-
-
-"""Options Views --> move own folder in future"""
-class OptionsView(TemplateView):
-    template_name = "league_admin/options.html"
-
-    def dispatch(self, request, *args, **kwargs):
-        self.league = League.objects.get(admin=self.request.user)
-
-        return super().dispatch(request, *args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['options'] = LeagueHittingOptions.objects.get(league=self.league)
-        return context
-
-
-class HittingOptionsFormView(FormView):
-    template_name = "league_admin/options.html"
-    form_class = LeagueHittingOptionsForm
-    success_url = reverse_lazy("league-admin-options")
-
-    def form_valid(self, form):
-        form.process()
-        return super().form_valid(form)
-
-class HittingOptionsUpdateView(PermissionRequiredMixin, UpdateView):
-    permission_required = 'league.league_admin'
-    template_name = "league_admin/option_templates/options_update.html"
-    form_class = LeagueHittingOptionsForm
-    model = LeagueHittingOptions
-    success_url = reverse_lazy("league-admin-options")
-
-"""hitting stat options"""
-class HittingStatsOptionsUpdateView(PermissionRequiredMixin, UpdateView):
-    """
-    Used for custom stats, gives options to turn on specific stats
-    for a more customizable website.
-    """
-    permission_required = 'league.league_admin'
-    template_name = "league_admin/option_templates/options_update.html"
-    form_class = LeagueHittingStatsOptionsForm
-    model = LeagueHittingStatsOptions
-    success_url = reverse_lazy("league-admin-hitting-options")
 
 
 
