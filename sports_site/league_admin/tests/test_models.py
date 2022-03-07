3f0b027149be7892588e713622baaa1afb7e4c74
Cole <cebCole@gmail.com>
2022-03-07 11:37:12 +0000

    commit fixing options tests work

--- sports_site/league_admin/tests/test_models.py
+++ sports_site/league_admin/tests/test_models.py
@@ -7,8 +7,11 @@ class LeagueHittingOptionsTestCase(TestCase):
     @classmethod
     def setUpTestaData(cls):
         cls.league = League.objects.get(id=1)
-        cls.league_hitting_options = LeagueHittingOptions.create(league=league)
+        cls.league_hitting_options = LeagueHittingOptions.objects.create(league=cls.league, stat_options=LeagueHittingOptions.SIMPLE)
 
     def test_created_properly(self):
+        print(League.objects.all())
+        print(LeagueHittingOptions.objects.all())
+        print(self.league_hitting_options)
         self.assertEqual(self.league_hitting_options.league, self.league)
         self.assertEqual(self.league_hitting_options.stat_options, LeagueHittingOptions.SIMPLE)
