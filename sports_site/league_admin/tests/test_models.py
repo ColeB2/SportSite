75218cc514fe16ee8787978903fbdc0ec7483836
Cole <cebCole@gmail.com>
2022-03-08 11:48:39 +0000

    basic tests for leaguehittingstatsOptions partially finished

--- sports_site/league_admin/tests/test_models.py
+++ sports_site/league_admin/tests/test_models.py
@@ -17,8 +17,8 @@ class LeagueHittingOptionsTestCase(TestCase):
         self.assertEqual(self.league_hitting_options.league, self.league)
 
     def test_labels(self):
-        league_label = self.league_hitting_optins._meta.get_field('league').verbose_name
-        options_label = self.league_hitting_optins._meta.get_field('stat_options').verbose_name
+        league_label = self.league_hitting_options._meta.get_field('league').verbose_name
+        options_label = self.league_hitting_options._meta.get_field('stat_options').verbose_name
         self.assertEqual(league_label, "league")
         self.assertEqual(options_label, "stat options")
 
@@ -28,3 +28,33 @@ class LeagueHittingOptionsTestCase(TestCase):
     def test_max_length(self):
         options_max_length = self.league_hitting_options._meta.get_field("stat_options").max_length
         self.assertEqual(options_max_length, 8)
+
+
+class LeagueHittingStatsOptionsTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.league = League.objects.get(id=1)
+        cls.lho = LeagueHittingOptions.objects.create(league=cls.league)
+        cls.lhso = LeagueHittingStatsOptions.objects.create(league=cls.league, league_options=cls.lho)
+
+
+    def test_fk_points(self):
+        self.assertEqual(self.lhso.league, self.league)
+        self.assertEqual(self.lhso.league_options, self.lho)
+
+    def test_labels(self):
+        label_dict = {"league": "league", "league_options": "league options",
+        "ordered_lineup": "Ordered", "at_bats": "AB", "plate_appearances": "PA",
+        "hits": "H", "runs": "R", "strikeouts": "SO", "walks": "BB",
+        "singles": "1B", "doubles": "2B", "triples": "3B", "homeruns": "HR",
+        "stolen_bases": "SB", "caught_stealing": "CS", "runs_batted_in": "RBI",
+        "hit_by_pitch": "HBP", "sacrifice_flies": "SF", "sacrifice_bunts": "SAC",
+        "average": "AVG", "on_base_percentage": "OBP", "slugging_percentage": "SLG",
+        "on_base_plus_slugging": "OPS", "reached_on_error": "ROE",
+        "fielders_choice": "FC", "intentional_walks": "IBB", "left_on_base": "LOB",
+        "picked_off": "PO", "ground_into_double_play": "GIDP",
+        "two_out_runs_batted_in": "2-out-RBI"
+        }
+        for k,v in label_dict.items():
+            label = self.lhso._meta.get_field(k).verbose_name
+            self.assertEqual(label, v)
