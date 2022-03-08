71c1a5cfeb731199ac3be9eeba218de643b55210
Cole <cebCole@gmail.com>
2022-03-08 10:58:58 +0000

    basic LeagueHittingOptionsTestCases complete

--- sports_site/league_admin/tests/test_models.py
+++ sports_site/league_admin/tests/test_models.py
@@ -5,13 +5,26 @@ from league.models import League
 
 class LeagueHittingOptionsTestCase(TestCase):
     @classmethod
-    def setUpTestaData(cls):
+    def setUpTestData(cls):
         cls.league = League.objects.get(id=1)
-        cls.league_hitting_options = LeagueHittingOptions.objects.create(league=cls.league, stat_options=LeagueHittingOptions.SIMPLE)
+        cls.league_hitting_options = LeagueHittingOptions.objects.create(league=cls.league)
 
     def test_created_properly(self):
-        print(League.objects.all())
-        print(LeagueHittingOptions.objects.all())
-        print(self.league_hitting_options)
         self.assertEqual(self.league_hitting_options.league, self.league)
         self.assertEqual(self.league_hitting_options.stat_options, LeagueHittingOptions.SIMPLE)
+
+    def test_fk_points(self):
+        self.assertEqual(self.league_hitting_options.league, self.league)
+
+    def test_labels(self):
+        league_label = self.league_hitting_optins._meta.get_field('league').verbose_name
+        options_label = self.league_hitting_optins._meta.get_field('stat_options').verbose_name
+        self.assertEqual(league_label, "league")
+        self.assertEqual(options_label, "stat options")
+
+    def test_defaults(self):
+        self.assertEqual(self.league_hitting_options.stat_options, LeagueHittingOptions.SIMPLE)
+
+    def test_max_length(self):
+        options_max_length = self.league_hitting_options._meta.get_field("stat_options").max_length
+        self.assertEqual(options_max_length, 8)
