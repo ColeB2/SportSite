6bd4407294199efdb477ac07cfe54834452254ee
Cole <cebCole@gmail.com>
2022-03-09 11:21:26 +0000

    new file to test Forms in LA

---
+++ sports_site/league_admin/tests/test_forms.py
@@ -0,0 +1,3 @@
+from django.test import TestCase
+from league_admin.forms import (LeagueHittingOptionsForm,
+    LeagueHittingStatsOptionsForm, CreateGameForm, EditGameForm, EditUserRosterPermissionsForm)
