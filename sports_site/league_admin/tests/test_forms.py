5d67b47d58a469421404a3b78502f2d6c076179f
Cole <cebCole@gmail.com>
2022-03-09 12:19:05 +0000

    form testing

--- sports_site/league_admin/tests/test_forms.py
+++ sports_site/league_admin/tests/test_forms.py
@@ -3,3 +3,15 @@ from league_admin.forms import (CreateGameForm, EditGameForm,
     LeagueHittingOptionsForm, LeagueHittingStatsOptionsForm,
     PlayerCreateForm, SeasonForm, SeasonStageCreateForm, TeamCreateForm,
     TeamSelectForm)
+
+
+class CreateGameFormTest(TestCase):
+    def test_create_game_form_labels(self):
+        form = CreateGameForm()
+        form_labels = {"home_team": "Home team", "away_team": "Away team",
+            "location": "Location (default: Home)", "date": None,
+            "start_time": "Start Time (24:00 clock)"}
+
+        for k,v in form_labels.items():
+            label = form.fields[k].label
+            self.assertTrue(label is None or label == v)
