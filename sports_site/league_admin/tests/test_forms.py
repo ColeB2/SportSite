d6e4a7ad421a8176a43d37f5eddd988843e4c221
Cole <cebCole@gmail.com>
2022-03-10 11:37:18 +0000

    basic creategameform test

--- sports_site/league_admin/tests/test_forms.py
+++ sports_site/league_admin/tests/test_forms.py
@@ -1,4 +1,6 @@
+import datetime
 from django.test import TestCase
+from league.models import TeamSeason
 from league_admin.forms import (CreateGameForm, EditGameForm,
     LeagueHittingOptionsForm, LeagueHittingStatsOptionsForm,
     PlayerCreateForm, SeasonForm, SeasonStageCreateForm, TeamCreateForm,
@@ -15,3 +17,15 @@ class CreateGameFormTest(TestCase):
         for k,v in form_labels.items():
             label = form.fields[k].label
             self.assertTrue(label is None or label == v)
+
+    def test_forms(self):
+        t1 = TeamSeason.objects.get(id="1")
+        t2 = TeamSeason.objects.get(id="2")
+        ts_query = TeamSeason.objects.all()
+        date = datetime.datetime(2022, 5, 11)
+        time = datetime.time(17, 00)
+        form_data = {"home_team": t1, "away_team": t2,
+            "date": date, "start_time": time}
+
+        form = CreateGameForm(data=form_data, team_queryset=ts_query)
+        self.assertTrue(form.is_valid())
