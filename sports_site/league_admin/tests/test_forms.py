0efe9c16381280a9aad66d4b4e78c054773afcf3
Cole <cebCole@gmail.com>
2022-03-14 09:56:16 +0000

    Finished basic editgameform test and player create form tests started

--- sports_site/league_admin/tests/test_forms.py
+++ sports_site/league_admin/tests/test_forms.py
@@ -1,6 +1,6 @@
 import datetime
 from django.test import TestCase
-from league.models import TeamSeason
+from league.models import Game, TeamSeason
 from league_admin.forms import (CreateGameForm, EditGameForm,
     LeagueHittingOptionsForm, LeagueHittingStatsOptionsForm,
     PlayerCreateForm, SeasonForm, SeasonStageCreateForm, TeamCreateForm,
@@ -32,7 +32,7 @@ class CreateGameFormTest(TestCase):
 
 
 class EditGameFormTest(TestCase):
-    def test_create_game_form_labels(self):
+    def test_edit_game_form_labels(self):
         form = EditGameForm()
         form_labels = {"home_team": "Home", "away_team": "Visitor",
             "location": "Location", "date": "Date",
@@ -42,3 +42,42 @@ class EditGameFormTest(TestCase):
         for k,v in form_labels.items():
             label = form.fields[k].label
             self.assertTrue(label is None or label == v)
+
+        print(form.model)
+
+    def test_forms(self):
+        t1 = TeamSeason.objects.get(id="1")
+        t2 = TeamSeason.objects.get(id="2")
+        date = datetime.datetime(2022, 5, 11)
+        time = datetime.time(17, 00)
+
+        form_data = {"home_team": t1, "away_team": t2,
+            "date": date, "start_time": time, "home_score": 99, "away_score": 0}
+
+        form = EditGameForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+
+class PlayerCreateFormTest(TestCase):
+    def test_player_create_form_labels(self):
+        form = PlayerCreateForm()
+        form_labels = {"first_name": "First name", "last_name": "Last name",
+            "birthdate": "Birthdate - YYYY-MM-DD format", "bats": "Bats",
+            "throw": "Throw", "height_feet": "Height, feet ie, 5,6 etc.",
+            "height_inches": "Height, inches ie 1,2,3... etc.",
+            "weight": "Weight, lbs"}
+
+        for k,v in form_labels.items():
+            label = form.fields[k].label
+            self.assertTrue(label is None or label == v)
+
+    def test_forms(self):
+        form_data = {"first_name": "Firsty", "last_name": "Lasty",
+            "birthdate": "1900-12-01", "bats": "Right", "throw": "Switch",
+            "height_feet": 7, "height_inches": 11, "weight": 400}
+
+        form = PlayerCreateForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+
+
