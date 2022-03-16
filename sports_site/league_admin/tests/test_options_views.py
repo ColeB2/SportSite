e1e3b7c7b6624ea37fadcca7e9a63fdc088ec40c
Cole <cebCole@gmail.com>
2022-03-16 10:34:16 +0000

    basic test_options view layout and first TestCase function made

---
+++ sports_site/league_admin/tests/test_options_views.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from league.models import Game, League, Season, SeasonStage, TeamSeason
+
+
+class LAOptionsViewTest(TestCase):
+    """
+    Tests OptionsView from league_admin/views/options_views.py
+
+    'options/hitting/<int:pk>/edit',
+    HittingOptionsUpdateView.as_view(),
+    name='league-admin-hitting-options-update'),
+    """
+    def test_view_without_logging_in(self):
+        response = self.client.get('/league/admin/options/hitting/1/edit')
+        self.assertEqual(response.status_code, 302)
