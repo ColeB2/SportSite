4614c0235749b8c600a27f0fb76e3e0c00da7a61
Cole <cebCole@gmail.com>
2022-03-22 05:23:10 -0500

    basic schedule select view testing complete

--- sports_site/league_admin/tests/test_schedule_views.py
+++ sports_site/league_admin/tests/test_schedule_views.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 
+from league.models import League, SeasonStage
+
 
 class LAScheduleSelectViewTest(TestCase):
     """
@@ -12,5 +14,36 @@ class LAScheduleSelectViewTest(TestCase):
     name='league-admin-schedule-select'
     """
     def test_view_without_logging_in(self):
-        response = self.client.get('/schedule')
+        response = self.client.get('/league/admin/schedule/')
         self.assertEqual(response.status_code, 302)
+
+    
+    def test_view_url_exists_at_desired_location(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get('/league/admin/schedule/')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_view_accessible_by_name(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get(reverse("league-admin-schedule-select"))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_view_uses_correct_template(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get(reverse("league-admin-schedule-select"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+            "league_admin/schedule_templates/schedule_select.html")
+
+
+    def test_context(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get(reverse("league-admin-schedule-select"))
+        self.assertEqual(response.status_code, 200)
+
+        stages = SeasonStage.objects.filter(
+            season__league=League.objects.get(id=1))
+        self.assertQuerysetEqual(
+            response.context["stages"], stages, ordered=False)
