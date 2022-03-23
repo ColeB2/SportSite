0adb1e2d459360135ff8440d846cf71d21be92b6
Cole <cebCole@gmail.com>
2022-03-23 05:37:09 -0500

    completed schedule create view tests sans succes url

--- sports_site/league_admin/tests/test_schedule_views.py
+++ sports_site/league_admin/tests/test_schedule_views.py
@@ -1,7 +1,8 @@
+from datetime import datetime
 from django.test import TestCase
 from django.urls import reverse
 
-from league.models import Game, League, SeasonStage
+from league.models import Game, League, SeasonStage, TeamSeason
 
 
 class LAScheduleSelectViewTest(TestCase):
@@ -105,3 +106,76 @@ class LAScheduleViewTest(TestCase):
             response.context["schedule"],
             schedule,
             ordered=False)
+
+
+class LAScheduleCreateViewTest(TestCase):
+    """
+    Tests league_admin_schedule_create_view
+        from league_admin/views/schedule_views.py
+
+    'schedule/<int:season_year>/stages/<season_stage_pk>/create',
+    views.league_admin_schedule_create_view,
+    name='league-admin-schedule-create'
+    """
+    def test_view_without_logging_in(self):
+        response = self.client.get('/league/admin/schedule/2022/stages/3/create')
+        self.assertEqual(response.status_code, 302)
+
+    
+    def test_view_url_exists_at_desired_location(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get('/league/admin/schedule/2022/stages/3/create')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_view_accessible_by_name(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get(reverse(
+            "league-admin-schedule-create",
+            kwargs={"season_year": 2022, "season_stage_pk": "3"}))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_view_uses_correct_template(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get(reverse(
+            "league-admin-schedule-create",
+            kwargs={"season_year": 2022, "season_stage_pk": "3"}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+            "league_admin/schedule_templates/schedule_create.html")
+
+
+    def test_context(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get(reverse(
+            "league-admin-schedule-create",
+            kwargs={"season_year": 2022, "season_stage_pk": "3"}))
+        self.assertEqual(response.status_code, 200)
+
+        stage = SeasonStage.objects.get(pk=3)
+        schedule = Game.objects.filter(season__pk="3")
+
+        self.assertTrue(response.context["formset"] is not None)
+        self.assertEqual(response.context["current_stage"], stage)
+
+
+    def test_success_url(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get(reverse(
+            "league-admin-schedule-create",
+            kwargs={"season_year": 2022, "season_stage_pk": "3"}))
+        self.assertEqual(response.status_code, 200)
+
+        resp = self.client.post(reverse(
+                "league-admin-schedule-create",
+                kwargs={"season_year": 2022, "season_stage_pk": "3"}),
+            {},
+            follow=True)
+        self.assertEqual(resp.status_code,200)
+        # self.assertRedirects(resp, reverse(
+        #     "league-admin-schedule",
+        #     kwargs={"season_year": 2022, "season_stage_pk": "3"})
+        # )
+        ##ToDo Figure out success url redirects.
+
