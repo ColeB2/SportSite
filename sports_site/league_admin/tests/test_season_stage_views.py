91384250257101dae8dd4a1c1778d3a3bb7f7c9e
Cole <cebCole@gmail.com>
2022-03-30 05:21:13 -0500

    LASeasonStageAddTeamsViewsTests basic tests complete

--- sports_site/league_admin/tests/test_season_stage_views.py
+++ sports_site/league_admin/tests/test_season_stage_views.py
@@ -298,6 +298,68 @@ class LASeasonStageDeleteInfoViewTest(TestCase):
         len_season_stage = len(SeasonStage.objects.all())
         self.assertEqual(len_season_stage, sslen)
 
+
+class LASeasonStageAddTeamsViewTests(TestCase):
+    """
+    Tests league_admin_season_stage_delete_info_view
+        from league_admin/views/season_stage_views.py
+
+    'season/<int:season_year>/<season_pk>/<season_stage_pk>/add/teams',
+    views.league_admin_season_stage_add_teams_view,
+    name='league-admin-season-stage-add-teams')
+    """
+    def test_view_without_logging_in(self):
+        response = self.client.get('/league/admin/season/2022/1/3/add/teams')
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_view_url_exists_at_desired_location(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get('/league/admin/season/2022/1/3/add/teams')
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_view_accessible_by_name(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get(reverse("league-admin-season-stage-add-teams",
+            kwargs={"season_year": 2022, "season_pk": "1",
+                "season_stage_pk": "1"}))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_view_uses_correct_template(self):
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get(reverse("league-admin-season-stage-add-teams",
+            kwargs={"season_year": 2022, "season_pk": "1",
+                "season_stage_pk": "1"}))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+            "league_admin/season_stage_templates/season_stage_add_teams.html")
+
+
+    def test_context(self):
+        season_year = 2022
+        season_pk = 1
+        season_stage_pk = 3
+        login = self.client.login(username="Test", password="test")
+        response = self.client.get(reverse("league-admin-season-stage-add-teams",
+            kwargs={"season_year": season_year, "season_pk": season_pk,
+                "season_stage_pk": season_stage_pk}))
+        self.assertEqual(response.status_code, 200)
+
+        season = Season.objects.get(pk=season_pk)
+        stage = SeasonStage.objects.get(pk=season_stage_pk)
+        teams = Team.objects.filter(league=League.objects.get(id=1))
+        exist_teams = TeamSeason.objects.filter(season__pk=season_stage_pk)
+
+
+        self.assertEqual(response.context["season"], season)
+        self.assertEqual(response.context["season_year"], season_year)
+        self.assertEqual(response.context["stage"], stage)
+        self.assertQuerysetEqual(
+            response.context["teams"], exist_teams, ordered=False)
+        self.assertTrue(response.context["formset"] is not None)
+
         
 
 
