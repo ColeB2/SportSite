501f8814d94b14b0e9ff04c26a1150c3a0cf7038
Cole <cebCole@gmail.com>
2022-04-04 04:54:28 -0500

    test_season_views.py creted and LASeasonViewTest basic tests complete

---
+++ sports_site/league_admin/tests/test_season_views.py
@@ -0,0 +1,50 @@
+from django.test import TestCase
+from django.urls import reverse
+
+from league.models import League, Season, SeasonStage
+
+
+class LASeasonViewTest(TestCase):
+    """
+    Tests league_admin_season_view
+        from league_admin/views/season_views.py
+
+    'season/',
+    views.league_admin_season_view,
+    name='league-admin-season')
+    """
+    def test_view_without_logging_in(self):
+        response = self.client.get('/league/admin/season/')
+        self.assertEqual(response.status_code, 302)
+        
+
+    def test_view_url_exists_at_desired_location(self):
+        self.client.login(username="Test", password="test")
+        response = self.client.get('/league/admin/season/')
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_view_accessible_by_name(self):
+        self.client.login(username="Test", password="test")
+        response = self.client.get(reverse("league-admin-season"))
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_view_uses_correct_template(self):
+        self.client.login(username="Test", password="test")
+        response = self.client.get(reverse("league-admin-season"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+            "league_admin/season_templates/season_page.html")
+
+
+    def test_context(self):
+        self.client.login(username="Test", password="test")
+        response = self.client.get(reverse("league-admin-season"))
+        self.assertEqual(response.status_code, 200)
+
+        league = League.objects.get(id=1)
+        seasons = Season.objects.filter(league=league)
+
+        self.assertQuerysetEqual(
+            response.context["seasons"], seasons, ordered=False)
