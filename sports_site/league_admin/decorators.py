5bcce6585de52fc413dfade8972e3243d66f39a2
Cole <cebCole@gmail.com>
2022-04-06 05:45:07 -0500

    decorators fix LA

--- sports_site/league_admin/decorators.py
+++ sports_site/league_admin/decorators.py
@@ -7,7 +7,10 @@ def user_owns_season_stage(function):
     @wraps(function)
     def wrap (request, *args, **kwargs):
         stage = SeasonStage.objects.get(pk=kwargs['season_stage_pk'])
-        if stage.season.league == request.user.userprofile.league:
+        if (
+            request.user.is_authenticated and
+            stage.season.league == request.user.userprofile.league
+            ):
             return function(request, *args, **kwargs)
         else:
             raise PermissionDenied
@@ -19,7 +22,10 @@ def user_owns_player(function):
     @wraps(function)
     def wrap (request, *args, **kwargs):
         player = Player.objects.get(pk=kwargs['player_pk'])
-        if player.league == request.user.userprofile.league:
+        if (
+            request.user.is_authenticated and 
+            player.league == request.user.userprofile.league
+            ):
             return function(request, *args, **kwargs)
         else:
             raise PermissionDenied
@@ -31,7 +37,10 @@ def user_owns_season(function):
     @wraps(function)
     def wrap (request, *args, **kwargs):
         season = Season.objects.get(pk=kwargs['season_pk'])
-        if season.league == request.user.userprofile.league:
+        if (
+            request.user.is_authenticated and
+            season.league == request.user.userprofile.league
+            ):
             return function(request, *args, **kwargs)
         else:
             raise PermissionDenied
@@ -43,7 +52,10 @@ def user_owns_team_season(function):
     @wraps(function)
     def wrap (request, *args, **kwargs):
         teamseason = TeamSeason.objects.get(pk=kwargs['team_season_pk'])
-        if teamseason.team.league == request.user.userprofile.league:
+        if (
+            request.user.is_authenticated and
+            teamseason.team.league == request.user.userprofile.league
+            ):
             return function(request, *args, **kwargs)
         else:
             raise PermissionDenied
@@ -55,7 +67,11 @@ def user_owns_team(function):
     @wraps(function)
     def wrap (request, *args, **kwargs):
         team = Team.objects.get(pk=kwargs['team_pk'])
-        if team.league == request.user.userprofile.league:
+        
+        if (
+            request.user.is_authenticated and
+            team.league == request.user.userprofile.league
+            ):
             return function(request, *args, **kwargs)
         else:
             raise PermissionDenied
