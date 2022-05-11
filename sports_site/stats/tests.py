84305c18b93a327b577d3499d54a83e7d1f2dcb0
Cole <cebCole@gmail.com>
2022-05-11 05:45:34 -0500

    deleted original tests.py file from stats app

--- sports_site/stats/tests.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from league.models import League
-
-# Create your tests here.
-class StatsTestCase(TestCase):
-
-    def test_herehere(self):
-        self.assertEqual(1,1)
-
