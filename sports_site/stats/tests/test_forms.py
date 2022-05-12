c5311430589627574b3e80058dc6e2bbc204fb6b
Cole <cebCole@gmail.com>
2022-05-12 05:12:23 -0500

    basic stats.form.PlayerStatsCreateForm Testing complete

--- sports_site/stats/tests/test_forms.py
+++ sports_site/stats/tests/test_forms.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from league.models import Game, SeasonStage, TeamSeason
 from stats.models import (PlayerHittingGameStats, PlayerPitchingGameStats,
     TeamGameStats, TeamGameLineScore)
 from stats.forms import (PlayerStatsCreateForm, PlayerPitchingStatsCreateForm,
@@ -6,9 +7,29 @@ from stats.forms import (PlayerStatsCreateForm, PlayerPitchingStatsCreateForm,
     PlayerPitchingGameStatsForm)
 
 
+
 class PlayerStatsCreateFormTest(TestCase):
-    def test_create_game_form_labels(self):
-        form = PlayerStatsCreateForm()
+    @classmethod
+    def setUpTestData(cls) -> None:
+        cls.stage = SeasonStage.objects.get(id=3)
+        cls.ts = TeamSeason.objects.get(id=1)
+        cls.game = Game.objects.get(id=1, home_team=cls.ts)
+
+        cls.tgs = TeamGameStats.objects.create(
+            season=cls.stage,
+            team=cls.ts,
+            game=cls.game
+        )
+        # cls.tgs = TeamGameStats.objects.get(season=cls.ts)
+        return super().setUpTestData()
+
+    def test_form_labels(self):
+        form = PlayerStatsCreateForm(
+            **{
+                "team_season": self.ts,
+                "team_game_stats": self.tgs
+            }
+        )
         form_labels = {"player": False}
 
         for k,v in form_labels.items():
@@ -16,13 +37,15 @@ class PlayerStatsCreateFormTest(TestCase):
             self.assertTrue(label is None or label == v)
 
     def test_forms(self):
-        pass
-        # t1 = TeamSeason.objects.get(id="1")
-        # t2 = TeamSeason.objects.get(id="2")
-        # ts_query = TeamSeason.objects.all()
-        # date = datetime.datetime(2022, 5, 11)
-        # time = datetime.time(17, 00)
-        # form_data = {"home_team": t1, "away_team": t2,
-        #     "date": date, "start_time": time}
-        # form = CreateGameForm(data=form_data, team_queryset=ts_query)
-        # self.assertTrue(form.is_valid())    
+        form_data = {"player": 1}   
+
+        form = PlayerStatsCreateForm(
+            data=form_data,
+            **{
+                "team_season": self.ts,
+                "team_game_stats": self.tgs
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+        
