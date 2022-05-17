c29c2ec4e19708afc18e4ee20831aba72e7de99f
Cole <cebCole@gmail.com>
2022-05-17 05:07:24 -0500

    stats TeamGameLinescoreCreateViewTest basic testing complete

--- sports_site/stats/tests/test_linescore_views.py
+++ sports_site/stats/tests/test_linescore_views.py
@@ -24,7 +24,7 @@ class TeamGameLinescoreCreateViewTest(TestCase):
         cls.league = League.objects.get(id=1)
         cls.stage = SeasonStage.objects.get(id=1)
         cls.team_season = TeamSeason.objects.get(id=1)
-        cls.game = Game.objects.get(id=1)
+        cls.game = Game.objects.get(id=2)
 
         cls.tgs = TeamGameStats.objects.create(
             season=cls.stage,
@@ -33,11 +33,36 @@ class TeamGameLinescoreCreateViewTest(TestCase):
         )
 
     def test_view_without_logging_in(self):
-        response = self.client.get('/league/stats/game/1/team/1/linescore/1')
+        response = self.client.get('/league/stats/game/2/team/1/linescore/1')
         self.assertEqual(response.status_code, 302)
 
 
     def test_view_url_exists_at_desired_location(self):
         self.client.login(username="Test", password="test")
-        response = self.client.get('/league/stats/game/1/team/1/linescore/1')
+        response = self.client.get('/league/stats/game/2/team/1/linescore/1')
         self.assertEqual(response.status_code, 302)
+
+    def test_view_accessible_by_name(self):
+        self.client.login(username="Test", password="test")
+        response = self.client.get(reverse('stats-linescore-create',
+            kwargs={
+                "game_pk": self.game.pk,
+                "team_season_pk": self.team_season.pk,
+                "team_game_stats_pk": self.tgs.pk
+            }))
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_redirects(self):
+        self.client.login(username="Test", password="test")
+        response = self.client.get(reverse('stats-linescore-create',
+            kwargs={
+                "game_pk": self.game.pk,
+                "team_season_pk": self.team_season.pk,
+                "team_game_stats_pk": self.tgs.pk
+            }))
+        self.assertRedirects(response, reverse("stats-team-game-stats", 
+            kwargs={
+                "game_pk": self.game.pk,
+                "team_season_pk": self.team_season.pk
+            }))
