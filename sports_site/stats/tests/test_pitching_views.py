53417358d53afa1974bc48db9efae8feaa16a506
Cole <cebCole@gmail.com>
2022-05-24 05:28:07 -0500

    team_game_pitching_stats_edit_view basic testing complete

--- sports_site/stats/tests/test_pitching_views.py
+++ sports_site/stats/tests/test_pitching_views.py
@@ -191,3 +191,157 @@ class TeamGamePitchingStatsCreateViewTest(TestCase):
         self.assertEqual(len(messages), 1)
         self.assertEqual(str(messages[0]),
             f'{ppgs.player.player} already has stats for {self.game}.')
+
+
+
+class TeamGamePitchingStatsEditViewTest(TestCase):
+    """
+    Tests team_game_pitching_stats_edit_view
+    from stats/views/tgs_pitching_views.py
+
+    'game/<int:game_pk>/team/<int:team_season_pk>/pitching/
+        <int:team_game_stats_pk>/edit',
+    views.team_game_pitching_stats_edit_view,
+    name='stats-game-pitching-stats-edit'
+    """
+    @classmethod
+    def setUpTestData(cls):
+        cls.league = League.objects.get(id=1)
+        cls.stage = SeasonStage.objects.get(id=3)
+        cls.team_season = TeamSeason.objects.get(id=1)
+        cls.game = Game.objects.get(id=2)
+        cls.roster = Roster.objects.get(team=cls.team_season)
+        cls.players = cls.roster.playerseason_set.all()
+
+        cls.tgs = TeamGameStats.objects.create(
+            season=cls.stage,
+            team=cls.team_season,
+            game=cls.game
+        )
+
+
+    def test_view_without_logging_in(self):
+        url = (
+            f"/league/stats/game/{self.game.pk}/team/{self.team_season.pk}/" +
+            f"pitching/{self.tgs.pk}/edit"
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+
+
+    def test_view_url_exists_at_desired_location(self):
+        self.client.login(username="Test", password="test")
+        url = (
+            f"/league/stats/game/{self.game.pk}/team/{self.team_season.pk}/" +
+            f"pitching/{self.tgs.pk}/edit"
+        )
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    
+    def test_view_accessible_by_name(self):
+        self.client.login(username="Test", password="test")
+        response = self.client.get(reverse('stats-game-pitching-stats-edit',
+            kwargs={
+                "game_pk": self.game.pk,
+                "team_season_pk": self.team_season.pk,
+                "team_game_stats_pk": self.tgs.pk,
+            }))
+        self.assertEqual(response.status_code, 200)
+
+
+    def test_view_uses_correct_template(self):
+        self.client.login(username="Test", password="test")
+        response = self.client.get(reverse('stats-game-pitching-stats-edit',
+            kwargs={
+                "game_pk": self.game.pk,
+                "team_season_pk": self.team_season.pk,
+                "team_game_stats_pk": self.tgs.pk,
+            }))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response,
+            "stats/game_pitching_stats_edit.html")
+    
+
+    def test_context(self):
+        self.client.login(username="Test", password="test")
+        response = self.client.get(reverse('stats-game-pitching-stats-edit',
+            kwargs={
+                "game_pk": self.game.pk,
+                "team_season_pk": self.team_season.pk,
+                "team_game_stats_pk": self.tgs.pk,
+            }))
+        self.assertEqual(response.status_code, 200)
+
+        self.assertEqual(response.context["game"], self.game)
+        self.assertEqual(response.context["team_season"],self.team_season)
+        
+        self.assertEqual(response.context["roster"], self.roster)
+        
+        self.assertQuerysetEqual(
+            response.context["players"], self.players, ordered=False)
+        self.assertTrue(response.context["formset"] is not None)
+        self.assertTrue(response.context["helper"] is not None)
+
+
+    def test_edit(self):
+        data= {
+            "playerpitchinggamestats_set-INITIAL_FORMS": 0,
+            "playerpitchinggamestats_set-TOTAL_FORMS": len(self.players),
+            "playerpitchinggamestats_set-MAX_NUM_FORMS": "",
+
+            #form 0
+            "playerpitchinggamestats_set-0-player": self.players[0].id,
+            "playerpitchinggamestats_set-0-win": 1,
+            "playerpitchinggamestats_set-0-innings_pitched": 9,
+        }
+        self.client.login(username="Test", password="test")
+        response = self.client.post(reverse('stats-game-pitching-stats-edit',
+            kwargs={
+                "game_pk": self.game.pk,
+                "team_season_pk": self.team_season.pk,
+                "team_game_stats_pk": self.tgs.pk,
+            }),
+            data=data,
+            follow=True)
+        
+        player = self.players[0]
+        ppgs = player.playerpitchinggamestats_set.get(team_stats=self.tgs)
+
+        self.assertEqual(ppgs.player, self.players[0])
+        self.assertEqual(ppgs.season, self.stage)
+        self.assertEqual(ppgs.team_stats, self.tgs)
+        self.assertEqual(ppgs.win, 1)
+
+        messages = list(response.context['messages'])
+        self.assertEqual(len(messages), 1)
+        self.assertEqual(str(messages[0]),
+            f'{ppgs} saved.')
+
+
+    def test_redirects(self):
+        data= {
+            "playerpitchinggamestats_set-INITIAL_FORMS": 0,
+            "playerpitchinggamestats_set-TOTAL_FORMS": len(self.players),
+            "playerpitchinggamestats_set-MAX_NUM_FORMS": "",
+
+            #form 0
+            "playerpitchinggamestats_set-0-player": self.players[0].id,
+            "playerpitchinggamestats_set-0-win": 1,
+            "playerpitchinggamestats_set-0-innings_pitched": 9,
+        }
+        self.client.login(username="Test", password="test")
+        response = self.client.post(reverse('stats-game-pitching-stats-edit',
+            kwargs={
+                "game_pk": self.game.pk,
+                "team_season_pk": self.team_season.pk,
+                "team_game_stats_pk": self.tgs.pk,
+            }),
+            data=data,
+            follow=True)
+        
+        self.assertRedirects(response, reverse("stats-team-game-stats",
+            kwargs={
+                "game_pk": self.game.pk,
+                "team_season_pk": self.team_season.pk
+            }))
