a070f644e1d5f36750d1fe416eb2dfa1e34f8768
Cole <cebCole@gmail.com>
2022-06-28 05:31:43 -0500

    docstring stats_tags, get_stage tag

--- sports_site/stats/templatetags/stats_tags.py
+++ sports_site/stats/templatetags/stats_tags.py
@@ -5,4 +5,11 @@ register = template.Library()
 
 @register.filter(name='get_stage')
 def get_stage(value):
+    """
+    Template tag used on the stats page, to get the the stage of a 
+    filtered season that otherwise wouldn't have been initially passed
+    onto the view and rendered.
+    Used in: pitching_stats_page.html, stats_page.html
+    team_pitching_stats_page.html, team_stast_page.html, standing_page.html.
+    """
     return SeasonStage.objects.get(pk=value)
