3dd748a543c56917cf0f90d7918cc5e1a1e6290a
Cole <cebCole@gmail.com>
2022-07-05 05:30:57 -0500

    basic stats.tests.test_stats_tags.py testing, 100% coverage

--- sports_site/stats/tests/test_stats_tags.py
+++ sports_site/stats/tests/test_stats_tags.py
@@ -1,5 +1,5 @@
 from django.template import Context, Template
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
 
 from league.models import League, SeasonStage, Season
@@ -14,49 +14,17 @@ class TestStatsTags(TestCase):
             context = Context(context)
             return Template(string).render(context)
 
+        league = League.objects.get(id=1)
+        stage = SeasonStage.objects.get(id=2)
+        factory = RequestFactory()
+        req = factory.get(f'/league/stats/?season={stage.pk}&league={league.url}')
+
         rendered = render_template(
             '{% load stats_tags %}'
             '{% if request.GET.season %} {{request.GET.season|get_stage}}'
-            '{% endif %}'
+            '{% endif %}',
+            {"league": league, "stage": stage, "request": req}
+
         )
-        print(rendered)
-        league = League.objects.get(id=1)
-        stage = SeasonStage.objects.get(id=3)
-        t = Template(
-            '{% load stats_tags %}'
-            '{% if request.GET.season %} {{request.GET.season|get_stage}}'
-            '{% endif %}'
-        )
-        c = Context({})
-        t.render(c)
-        # print(c['season'])
-        # print('XXX')
-        # print(t)
-        self.client.login(username="Test", password="test")
-        response = self.client.get(reverse(
-            'stats-page')+f'?league={league.url}')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "stats/stats_page.html")
-        # print(response)
-
-
-    def test_other_season(self):
-        """from test_views, tinkering."""
-        league = League.objects.get(id=1)
-        stage = SeasonStage.objects.get(id=2)
-        self.client.login(username="Test", password="test")
-        response = self.client.get(reverse(
-            'stats-page') + f'?season={stage.pk}&league={league.url}')
-        self.assertEqual(response.status_code, 200)
-
-
-        #Tests get_state tag, which should change the title of the table
-        title = (
-            f'<h3 class="text-center">' +
-            f'{stage}'+
-            f'</h3>'
-            )
-
-        self.assertInHTML(title, response.content.decode())
-        self.assertInHTML("2022 Postseason", response.content.decode())
+        self.assertIn(str(stage), rendered)
 
